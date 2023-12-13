75be7b8c21859e168d7d0381cd8081184043de2a
denny <dennyvdstarre@gmail.com>
2023-12-13 16:25:48 +0100

    Version 1.2, bestiary info showing make insert function next

--- dndtools/Bestiary.php
+++ dndtools/Bestiary.php
@@ -12,50 +12,61 @@
 
     <?php
     $connection = mysqli_connect("localhost","root", "", "dnddb", "3306");
-   $sql = 'select C.idCreature, Creaturename, Creaturetype, Dangerlevel, Hp, AC, Speed, Str, Dex, Con, `Int`, Wis, Cha, `Saving throws`, Senses, Skills, Vulnerabilities, Resistances, Immunities, Actionname, Actiontype, A.Description, C.Description from creatures C join actcre AC on C.idCreature = AC.idCreature join actions A on AC.idAction = A.idAction';
+   $sql = 'select idCreature, Creaturename, Creaturetype, Dangerlevel, Hp, AC, Speed, Str, Dex, Con, `Int`, Wis, Cha, `Saving throws`, Senses, Skills, Vulnerabilities, Resistances, Immunities, Creaturedescription from creatures';
 $result = mysqli_query($connection, $sql);
     if(mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
         echo '<details>';
         echo '<summary>';
-        echo '<p><b>'.$row['Creaturename'].'</b></p>';
+        echo '<b>'.$row['Creaturename'].'</b>';
         echo '</summary>';
-        echo '<p>'.'Creature Id:'.$row['idCreature'].'</p>';
-        echo '<p>'.$row['Creaturetype'].'</p>';
-        echo '<p>'.$row['Dangerlevel'].'</p>';
-        echo '<p>'.'Armor Class: '.$row['AC'].'</p>';
-        echo '<p>'.'Hit Points: '.$row['Hp'].'</p>';
-        echo '<p>'.'Speed: '.$row['Speed'].'</p>';
-        echo '<p>'.'STR:'.$row['Str'];
-        echo '     DEX:'.$row['Dex'];
-        echo '     CON:'.$row['Con'];
-        echo '     INT:'.$row['Int'];
-        echo '     WIS:'.$row['Wis'];
-        echo '     CHA:'.$row['Cha'].'</p>';
-        echo '<p>'.'<b>Saving throws</b> '.$row['Saving throws'].'</p>';
-        echo '<p>'.'Skills '.$row['Skills'].'</p>';
-        echo '<p>'.'Senses '.$row['Senses'].'</p>';
-        echo '<p>'.'Vulnerabilities '.$row['Vulnerabilities'].'</p>';
-        echo '<p>'.'Resistances '.$row['Resistances'].'</p>';
-        echo '<p>'.'Immunities '.$row['Immunities'].'</p>';
-        while(!empty($row['Actiontype'])) {
-            echo $row['Actiontype'];
-            echo $row['Actionname'];
-            echo $row['A.Description'];
+        echo "______________________________________________________________________________";
+        $id = $row['idCreature'];
+        echo '<p><b>'.'Creature id: </b>'.$id.'</p>';
+        echo '<p><b>'.'Creature type: </b>'.$row['Creaturetype'].'</p>';
+        echo '<p><b>'.'Danger level: </b>'.$row['Dangerlevel'].'</p>';
+        echo "______________________________________________________________________________";
+        echo '<p><b>'.'Armor Class: </b>'.$row['AC'].'</p>';
+        echo '<p><b>'.'Hit Points: </b>'.$row['Hp'].'</p>';
+        echo '<p><b>'.'Speed: </b>'.$row['Speed'].'</p>';
+        echo "______________________________________________________________________________";
+        echo '<p><b>'.'STR: </b>'.$row['Str'];
+        echo '<b> DEX: </b>'.$row['Dex'];
+        echo '<b> CON: </b>'.$row['Con'];
+        echo '<b> INT: </b>'.$row['Int'];
+        echo '<b> WIS: </b>'.$row['Wis'];
+        echo '<b> CHA: </b>'.$row['Cha'].'</p>';
+        echo "______________________________________________________________________________";
+        echo '<p><b>'.'Saving throws: </b>'.$row['Saving throws'].'</p>';
+        echo '<p><b>'.'Skills: </b>'.$row['Skills'].'</p>';
+        echo '<p><b>'.'Senses: </b>'.$row['Senses'].'</p>';
+        echo '<p><b>'.'Vulnerabilities: </b>'.$row['Vulnerabilities'].'</p>';
+        echo '<p><b>'.'Resistances: </b>'.$row['Resistances'].'</p>';
+        echo '<p><b>'.'Immunities: </b>'.$row['Immunities'].'</p>';
+        $Cd = $row['Creaturedescription'];
+        $sql = "select Actiontype from Actions A join actcre AC on A.idAction = AC.idAction WHERE AC.idCreature = $id GROUP BY Actiontype ASC";
+        $result1 = mysqli_query($connection, $sql);
+        if(mysqli_num_rows($result1) > 0) {
+            while($row = mysqli_fetch_assoc($result1)) {
+                echo "______________________________________________________________________________";
+                $at = $row['Actiontype'];
+                echo '<h1><b>'.$at.'</b></h1>';
+    $sql = "select Actionname, Actiondescription from Actions A join actcre AC on A.idAction = AC.idAction WHERE idCreature = $id AND A.Actiontype = '$at' ORDER BY Actionname ASC";
+    $result2 = mysqli_query($connection, $sql);
+    if(mysqli_num_rows($result2) > 0) {
+        while($row = mysqli_fetch_assoc($result2)) {
+            echo '<p><b>'.$row['Actionname'].'</b>   '.$row['Actiondescription'].'</p>';
         }
-        echo '<p>'.$row['C.Description'].'</p>';
+    }
+    }}
+        echo "______________________________________________________________________________";
+        echo '<p>'.$Cd.'</p>';
+        echo "______________________________________________________________________________";
         echo '</details>';
+
     }}
     mysqli_close($connection);
     ?>
-</table>
-<p id="demo"></p>
-<script>
-    function OnClick() {
-
-    }
-</script>
-
 
 
 
---
+++ dndtools/CreatureCreation.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Index</title>
+<body>
+<a href="Bestiary.php">
+    <button>Bestiary</button>
+</a>
+<a href="Characters.php">
+    <button>Characters</button>
+</a>
+<a href="Music.php">
+    <button>Music</button>
+</a>
+<a href="Locations.php">
+    <button>Locations</button>
+</a>
+<a href="Stories.php">
+    <button>Stories</button>
+</a>
+<a href="Rules.php">
+    <button>Rules</button>
+</a>
+<a href="Items.php">
+    <button>Items</button>
+</a>
+<a href="DMing.php">
+    <button>DMing</button>
+</a>
+<a href="Spells.php">
+    <button>Spells</button>
+</a>
+
+
+
+
+</body>
+</html>
