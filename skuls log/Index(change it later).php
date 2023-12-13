cb6ad773042625597e193d46caa8f05fea55c146
tnttime1 <tnttime1@gmail.com>
2023-12-13 15:38:06 +0100

    fu

---
+++ skuls log/Index(change it later).php
@@ -0,0 +1,366 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="32x32" href="/icons/tntmainpageicon.png">
+    <title>skuls log</title>
+    <!-- The style.css file allows you to change the look of your web pages.
+         If you include the next line in all your web pages, they will all share the same look.
+         This makes it easier to make new pages for your site. -->
+    <link href="/style.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body>
+<div id="container"><div id="topBar"></div><div></div>
+    <div id="headerArea"> <!--<img src="https://pbs.twimg.com/media/EFLVwkeX4AAF_aO.jpg" width = 100%; height = auto;>-->
+
+        <!-- link bar on top of page -->
+        <nav id="navbar" style="margin-bottom: 0px;"><ul>                                          <!--honk-->
+                <li><a href="#"> Home </a></li>
+                <li><a href="https://www.pixilart.com/tnttime1" target="_blank"> Link Art </a></li>
+                <li><a href="#"> Link More Mess </a></li>
+                <li><a href="/thoughts/index.html"> Link My Thoughts </a></li>
+                <li><a href="#"> Link art2 </a></li>
+                <li><a href="#">Lonk maybe games </a></li>
+            </ul></nav></div>
+
+    <div id="flex">
+        <aside id="leftSidebar" style="margin-right: 0px;">
+            <!--this is the left bar-->
+            <h2>new stuff</h2>
+            <div class="box">
+                <p>created on 31 december 2022 on a cool <a href="https://neocities.org">site</a></p>
+                <ul style="padding-left:10px;">
+                    <li>created this site</li>
+                    <li>linked stuff to other sites</li>
+                    <li>had fun</li>
+                </ul>
+            </div>
+            <h2>sup</h2>
+            <p>this is a me zone have a look or do smth idk. maybe add some cool suggestions into the suggestion box... when i make one</p>
+            <h3>Other Tools For Making Your Page</h3>
+            <ul>
+                <li><a href="https://sadgrlonline.github.io/html-cheatsheet/" target="_blank">HTML Cheatsheet</a>
+                </li><li><a href="https://sadgrl.online/webmastery/webmaster-links.html" target="_blank">Webmaster Links</a></li>
+                <li><a href="https://sadgrl.online/webmastery/downloads/tiledbgs.html" target="_blank">Tiled BGs</a></li>
+                <li><a href="https://sadgrl.online/webmastery/downloads/fonts.html" target="_blank">Fonts</a></li>
+                <li><a href="https://sadgrl.online/projects/ideas/" target="_blank">Site Ideas</a></li>
+                <li><a href="https://sadgrl.online/learn/sections/html-and-css" target="_blank">Guides</a></li>
+            </ul>
+        </aside>
+        <!--this is the main zone ie the middle bar-->
+        <main>
+            <h1>hey human welcome to my work in progress site</h1>
+            <p><strong>this is a lil home away from home</strong></p>
+            <p> im making a website with <a href="https://learn.sadgrl.online/understanding-tweaking-my-layout-maker/" target="_blank">this guide</a> which has helped me a lot.itll have some moving stars and swaying trees, im gonna have a few that are just nice places to be with nice vibes maybe their own radios and activities or games also i see why i never have time XD. i keep using it all doing things i like but not leaving much extra for other things i like. i was gonna rewatch ok ko but i spent like two days on this so far. school exists and im ok with it rn its kinda given me all it can and now its just about passing exams before i can fully just drain my time into whatever i feel like at the moment</p>
+
+            <p>im the kind of kid to have 100 hobbies and be good at many of them but never spend much time on any or really perfecting one XD. worms are cool tho and so is magic especially ones with trees </p>
+            <img src="/my_path.jpg" width=300px; height=auto;>
+
+            <p>check the links in the sidebar for resources to build your own website!</p>
+
+            <p> *brain box* </p>
+            <p>time is zooming by and it feels like a breeze running through me its nice even if it is short</p>
+
+            <details>
+                <summary>click me for more stuff idk </summary>
+                <p>here is a cool video : ) yippie it works!</p>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/O8M2I3Fs8gw?autoplay=1&modestbranding=1" title="YouTube video player">
+                </iframe></details>
+
+            <p>hey its me again form 2023 this time so the update is pretty much that ima make a pesterchum theme and that i use pesterchum to talk to me also i should def make a thought zonw or likw an update page</p>
+
+
+        </main>
+        <!--right side bar-->
+        <aside id="rightSidebar" style="margin-left: 0px;"><h2>goals</h2>
+            <div class="box">
+                <p>here are some things i wanna do</p>
+                <ul style="padding-left:10px;">
+                    <li>link it to other sites something like a webring</li>
+                    <li>make a secondary site and mess around with that</li>
+                    <li>make floating buttons that go ontop of stuff</li>
+                    <li>add a radio to the site</li>
+                    <li>make suggestion box</li>
+                    <li>have fun</li>
+                </ul>
+            </div>
+            <h2>stuff i like and have consumed</h2>
+            <p>i may have been in a few fandoms but i never really chat in em</p>
+            <p>homestuck,vast error,over the garden wall,steven universe,jojo,adventure time,mob psycho,one punch man,re zero,overlord,teen titans,yume nikki,k******,    ,one piece,king of the hill,amphibia,the owl house,ok ko lets be heroes,arcane,the hollow a lot basically</p>
+        </aside></div>
+
+    <footer id="footer" style="margin-top: 0px;"></footer></div>
+<!-- THIS IS THE CSS !-->
+<style>
+    /* user styles */
+
+    /* styles are what change the color and sizes of stuff on your site. */
+
+    /* these are variables that are being used in the code
+these tended to confuse some people, so I only kept
+the images as variables */
+
+
+    :root {
+        --header-image: url('https://pbs.twimg.com/media/EFLVwkeX4AAF_aO.jpg');
+        --body-bg-image: url('/over_the_garden_wall_forest.png');
+        /* colors */
+        --content: #3FFF00;
+    }
+
+    /* if you have the URL of a font, you can set it below */
+    /* feel free to delete this if it's not your vibe */
+
+    /* this seems like a lot for just one font and I would have to agree
+but I wanted to include an example of how to include a custom font.
+If you download a font file you can upload it onto your Neocities
+and then link it! Many fonts have separate files for each style
+(bold, italic, etc. T_T) which is why there are so many!
+
+*/
+
+    @font-face {
+        font-family: Nunito;
+        src: url('https://sadhost.neocities.org/fonts/Nunito-Regular.ttf');
+    }
+
+    @font-face {
+        font-family: Nunito;
+        src: url('https://sadhost.neocities.org/fonts/Nunito-Bold.ttf');
+        font-weight: bold;
+    }
+
+    @font-face {
+        font-family: Nunito;
+        src: url('https://sadhost.neocities.org/fonts/Nunito-Italic.ttf');
+        font-style: italic;
+    }
+
+    @font-face {
+        font-family: Nunito;
+        src: url('https://sadhost.neocities.org/fonts/Nunito-BoldItalic.ttf');
+        font-style: italic;
+        font-weight: bold;
+    }
+
+    body {
+        font-family: 'Nunito', sans-serif;
+        margin: 0;
+        background-color: #8B410B;
+        /* you can delete the line below if you'd prefer to not use an image */
+        background-size: 1500px;
+        color: #F4AD20;
+        background-image: var(images!/autumnforestbackground.png);
+    }
+
+    * {
+        box-sizing: border-box;
+    }
+
+    /* below this line is CSS for the layout */
+
+    /* this is a CSS comment
+to uncomment a line of CSS, remove the * and the /
+before and after the text */
+
+
+    /* the "container" is what wraps your entire website */
+    /* if you want something (like the header) to be Wider than
+the other elements, you will need to move that div outside
+of the container */
+    #container {
+        max-width: 900px;
+        /* this is the width of your layout! */
+        /* if you change the above value, scroll to the bottom
+and change the media query according to the comment! */
+        margin: 0 auto;
+        /* this centers the entire page */
+    }
+
+    /* the area below is for all links on your page
+EXCEPT for the navigation */
+    #container a {
+        color: #ED6E10;
+        font-weight: bold;
+        /* if you want to remove the underline
+you can add a line below here that says:
+text-decoration:none; */
+    }
+
+    #header {
+        width: 100%;
+        background-color: #8B410B;
+        /* header color here! */
+        height: 150px;
+        /* this is only for a background image! */
+        /* if you want to put images IN the header,
+you can add them directly to the <div id="header"></div> element! */
+        background-image: var(--header-image);
+        background-size: 100%;
+    }
+
+    /* navigation section!! */
+    #navbar {
+        height: 40px;
+        background-color: #70360B;
+        /* navbar color */
+        width: 100%;
+    }
+
+    #navbar ul {
+        display: flex;
+        padding: 0;
+        margin: 0;
+        list-style-type: none;
+        justify-content: space-evenly;
+    }
+
+    #navbar li {
+        padding-top: 10px;
+    }
+
+    /* navigation links*/
+    #navbar li a {
+        color: #ED6E10;
+        /* navbar text color */
+        font-weight: 800;
+        text-decoration: none;
+        /* this removes the underline */
+    }
+
+    /* navigation link when a link is hovered over */
+    #navbar li a:hover {
+        color: #a49cba;
+        text-decoration: underline;
+    }
+
+    #flex {
+        display: flex;
+    }
+
+    /* this colors BOTH sidebars
+if you want to style them separately,
+create styles for #leftSidebar and #rightSidebar */
+    aside {
+        background-color: #142445;
+        width: 200px;
+        padding: 20px;
+        font-size: smaller;
+        /* this makes the sidebar text slightly smaller */
+    }
+
+
+    /* this is the color of the main content area,
+between the sidebars! */
+    main {
+        background-color: #FFEFD2;/*                                 change color of the background      */
+        flex: 1;
+        padding: 20px;
+        order: 2;
+    }
+
+    /* what's this "order" stuff about??
+allow me to explain!
+if you're using both sidebars, the "order" value
+tells the CSS the order in which to display them.
+left sidebar is 1, content is 2, and right sidebar is 3! */
+
+    */ #leftSidebar {
+           order: 1;
+       }
+
+    #rightSidebar {
+        order: 3;
+    }
+
+    footer {
+        background-color: #13092D;
+        /* background color for footer */
+        width: 100%;
+        height: 40px;
+        padding: 10px;
+        text-align: center;
+        /* this centers the footer text */
+    }
+
+    h1,
+    h2,
+    h3 {
+        color: #FC500B;
+    }
+
+    h1 {
+        font-size: 25px;
+    }
+
+    strong {
+        /* this styles bold text */
+        color: #FF5733;
+    }
+
+    /* this is just a cool box, it's the darker colored one */
+    .box {
+        background-color: #FFE5D7;/*                                         i really have to work on colors */
+        border: 1px solid #D13C00;/*                                         for both box and outline of the box  */
+        padding: 10px;
+    }
+
+    /* CSS for extras */
+
+    #topBar {
+        width: 100%;
+        height: 30px;
+        padding: 10px;
+        font-size: smaller;
+        background-color: #13092D;
+    }
+
+
+    /* BELOW THIS POINT IS MEDIA QUERY */
+
+    /* so you wanna change the width of your page?
+by default, the container width is 900px.
+in order to keep things responsive, take your new height,
+and then subtrack it by 100. use this new number as the
+"max-width" value below
+*/
+
+
+    @media only screen and (max-width: 800px) {
+
+        #flex {
+            flex-wrap: wrap;
+        }
+
+        aside {
+            width: 100%;
+        }
+
+        /* the order of the items is adjusted here for responsiveness!
+since the sidebars would be too small on a mobile device.
+feel free to play around with the order!
+*/
+
+        main {
+            order: 1;
+        }
+
+        #leftSidebar {
+            order: 2;
+        }
+
+        #rightSidebar {
+            order: 3;
+        }
+
+        #navbar ul {
+            flex-wrap: wrap;
+        }
+
+
+
+    }
+</style>
+</body>
+</html>
